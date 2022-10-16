0e9c3d74b2a6f68d834c1a85b8b4a611cda49eaf
Ferigit <faramarz.razmi@gmail.com>
2022-10-16 22:14:46 +0330

    add material ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,34 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "../src/store/store";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
 
-function MyApp({ Component, pageProps }: AppProps) {
+import createEmotionCache from "../src/utility/createEmotionCache";
+import lightThemeOptions from "../styles/theme/lightThemeOptions";
+import "../styles/globals.css";
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+const clientSideEmotionCache = createEmotionCache();
+
+const lightTheme = createTheme(lightThemeOptions);
+
+function MyApp({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}: MyAppProps) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
